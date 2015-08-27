431796d392db93e9f030535d6073f35100f50bc1
feca na <fecanavarro@hotmail.com>
2015-08-27 16:23:18 +0200

    Prueba inicial con Httrack manual

---
+++ Themes/AdventuresTheme/scripts/Custom.js
@@ -0,0 +1,20 @@
+﻿$(window).load(function () {
+    var ltIE9 = $('#lt-ie9')[0];
+    if (!ltIE9) {
+        $('#container').masonry({
+            columnWidth: 100,
+            itemSelector: '.item',
+            gutter: 10,
+        });
+    }
+});
+
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        items: 3,
+    });
+
+    //$(".page h2, .contacto h2, .blog h2, .blog-post h2, .actividad-aventura h2, .bici h2, .quad h2, .alojamiento-page h2")
+    //    .prepend('<i class="fa fa-level-down" style="margin-right:10px" ></i>');
+});
