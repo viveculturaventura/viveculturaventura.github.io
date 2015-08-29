a3fc0e4bad519846ba76ec856fed1d6c450e5fee
feca na <fecanavarro@hotmail.com>
2015-08-29 17:29:58 +0200

    Poner carpeta "Adventures" para que no haya que redirigir

--- Themes/AdventuresTheme/scripts/langdir.js
+++ Themes/AdventuresTheme/scripts/langdir.js
@@ -1,11 +1,5 @@
 var langcodes = ["fr", "de", "ca", "it", "en"];
 
-// Browser Language Redirect script
-// copyright 3rd January 2006, Stephen Chapman
-// permission to use this Javascript on your web page is granted
-// provided that all of the code in this script (with the sole exception
-// of the langcodes array entries) is used without any alteration
-
 var langCode = navigator.language || navigator.systemLanguage;
 var lang = langCode.toLowerCase();
 lang = lang.substr(0, 2);
